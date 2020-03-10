4e0e9c046bac4cb84f4323e04e121849912b8bf1
Duc Le <duc.le@stfc.ac.uk>
2020-03-10 07:47:23 +0000

    Refactor call_matlab.mex to use C API
    Former-commit-id: a6580e62c2491287944ee7ec60ad88186ea89b0f

--- src/call_matlab.cpp
+++ src/call_matlab.cpp
@@ -1,23 +1,14 @@
-#include "mex.hpp"
-#include "mexAdapter.hpp"
+#include "mex.h"
 #include "pybind11/pybind11.h"
 
 namespace py = pybind11;
 
-using matlab::mex::ArgumentList;
-class MexFunction : public matlab::mex::Function {
-
-    public:
-        void operator()(ArgumentList outputs, ArgumentList inputs) {
-        }
-};
-
-void call_matlab(uint64_t engine_addr, std::string command) {
-    std::shared_ptr<matlab::engine::MATLABEngine> matlab_ptr = std::make_shared<matlab::engine::MATLABEngine>((void*)engine_addr);
-    matlab_ptr->eval(matlab::engine::convertUTF8StringToUTF16String(command));
+void call_matlab(std::string command) {
+    mexEvalString(command.c_str());
+    //mexCallMATLAB(0, NULL, 0, NULL, command.c_str());
 }
 
 PYBIND11_MODULE(call_matlab, m) {
     m.def("call_matlab", &call_matlab, "A function to call matlab with an arbitrary string",
-          py::arg("engine"), py::arg("command"));
+          py::arg("command"));
 }
