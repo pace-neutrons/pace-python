9d5879d62f0f329a6caa8186a9128bc0acb4e2c7
Duc Le <duc.le@stfc.ac.uk>
2021-01-21 19:35:25 +0000

    Clean up uneeded files

--- extlib/compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-gcc -shared -o fe_sqw_c.so -fPIC fe_sqw.c
-g++ -shared -o fe_sqw_cpp.so -fPIC fe_sqw.cpp
-gfortran -shared -o fe_sqw_f.so -fPIC fe_sqw.f90
-
-gcc -shared -o fe_sqw_struct_c.so -fPIC fe_sqw_struct.c
-gfortran -shared -o fe_sqw_struct_f.so -fPIC fe_sqw_struct.f90
-gfortran -shared -o fe_sqw_struct_f_init.so -fPIC fe_sqw_struct_init.f90
