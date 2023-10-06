8784f91fb2d8a025c006bcef0fe7bc7eeb3318fe
Larkins <aaron.larkins@stfc.ac.uk>
2023-10-06 14:11:41 +0100

    Added new variables to root CMakeLists.txt. Updated PACE.cmake to incorporate these.

--- cmake/PACE.cmake
+++ cmake/PACE.cmake
@@ -4,19 +4,19 @@ message(STATUS "Downloading PACE")
 
 download(
     PROJ SPINW
-    GIT_REPOSITORY https://github.com/mducle/spinw.git
-    GIT_TAG e3e57aae432f6737aff81d31ccda6e7dce741cef
+    GIT_REPOSITORY https://github.com/${SPINW_REPO}.git
+    GIT_TAG ${SPINW_VERSION}
     GIT_SHALLOW 1
 )
 
 if(WIN32)
     download(
         PROJ HORACE
-        URL https://github.com/pace-neutrons/Horace/releases/download/v3.6.2/Horace-3.6.2-win64-R2019b.zip
+        URL https://github.com/pace-neutrons/Horace/releases/download/v${HORACE_VERSION}/Horace-${HORACE_VERSION}-win64-R2019b.zip
     )
 else()
     download(
         PROJ HORACE
-        URL https://github.com/pace-neutrons/Horace/releases/download/v3.6.2/Horace-3.6.2-linux-R2019b.tar.gz
+        URL https://github.com/pace-neutrons/Horace/releases/download/v${HORACE_VERSION}/Horace-${HORACE_VERSION}-linux-R2019b.tar.gz
     )
 endif()
