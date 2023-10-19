09a9d1a0b55b6eed49cde65ac1085df1887c7234
Aaron Larkins <aaron.larkins@stfc.ac.uk>
2023-10-19 15:39:57 +0100

    Add working download of SpinW

--- cmake/PACE.cmake
+++ cmake/PACE.cmake
@@ -1,34 +1,49 @@
 include(Download)
 include(ExternalProject)
 
-#TODO: update the message below to make it suitable for the new logic.
-message(STATUS "Downloading PACE") 
+message(STATUS "Obtaining desired PACE components") 
 
 if(WITH_SPINW)
     if(SPINW_PATH)
         #TODO: Set ExternalProject_Add up correctly for SpinW
         #ISSUE: Currently does not actualy have anyway to check if SpinW is in the dir specified
+        message(STATUS "Including existing SpinW")
         ExternalProject_Add(SpinW 
             SOURCE_DIR "${SPINW_PATH}"
             #INSTALL_DIR "${CMAKE_CURRENT_BINARY_DIR}/CTF"
             DOWNLOAD_COMMAND ""
-            #INSTALL_COMMAND ""
+            CONFIGURE_COMMAND ""
+            BUILD_COMMAND "" 
+            INSTALL_COMMAND ""
+            #CMAKE_ARGS "-DCMAKE_INSTALL_PREFIX=${CMAKE_CURRENT_BINARY_DIR}/CTF/SpinW"
             )
         #message(FATAL_ERROR "Successfully included ${SPINW_PATH}") #check this
     else()
         message(STATUS "Downloading SpinW")
-        download(
-            PROJ SPINW
+        ExternalProject_Add(SpinW
             GIT_REPOSITORY https://github.com/${SPINW_REPO}.git
             GIT_TAG ${SPINW_VERSION}
             GIT_SHALLOW 1
+            BINARY_DIR "${CMAKE_CURRENT_BINARY_DIR}/CTF/SpinW"
+            CONFIGURE_COMMAND ""
+            BUILD_COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/swfiles <SOURCE_DIR>/external <SOURCE_DIR>/dat_files <BINARY_DIR> 
+            INSTALL_COMMAND ""
+            TEST_COMMAND ""
         )
+        
+        # download(
+        #     PROJ SPINW
+        #     GIT_REPOSITORY https://github.com/${SPINW_REPO}.git
+        #     GIT_TAG ${SPINW_VERSION}
+        #     GIT_SHALLOW 1
+        #     SOURCE_DIR "${CMAKE_CURRENT_BINARY_DIR}/CTF" 
+        #)
     endif()
 endif()
 
-#TODO: Update this so that its inline with spinw above once it is working
 if(WITH_HORACE)
     if(HORACE_PATH)
+        message(STATUS "Including existing Horace")
         ExternalProject_Add(HORACE 
             SOURCE_DIR ${HORACE_PATH}
             #INSTALL_DIR "${CMAKE_CURRENT_BINARY_DIR}/CTF"
