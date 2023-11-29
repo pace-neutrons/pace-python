e62cb6af76f7004e9740f189a45a11d41bb2a862
Aaron Larkins <aaron.larkins@stfc.ac.uk>
2023-11-29 14:21:09 +0000

    Update copy_and_remove_0.sh

--- cmake/copy_and_remove_0.sh
+++ cmake/copy_and_remove_0.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-
-if [ ! -d $2 ]
-then
-    mkdir "$2"
-fi
-
-cp -r "$1" "$2"
-find "$2" -name _docify -exec rm -rf '{}' \;
-find "$2" -name .git -exec rm -rf '{}' \;
-find "$2" -size 0 -exec rm -f '{}' \;
-find "$2" -name "@*_old" -exec rm -rf '{}' \;
+find "$1" -name _docify -exec rm -rf '{}' \;
+find "$1" -name .git -exec rm -rf '{}' \;
+find "$1" -size 0 -exec rm -f '{}' \;
+find "$1" -name "@*_old" -exec rm -rf '{}' \;
 
 exit 0
