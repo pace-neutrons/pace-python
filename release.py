5c42688a5f9aa752a37333f1a5e287d605b42f75
Aaron Larkins <aaron.larkins@stfc.ac.uk>
2024-02-23 14:37:27 +0000

    Update regex for obtaining release desc

--- release.py
+++ release.py
@@ -36,7 +36,7 @@ def release_github(test=True):
         if pace_ver != ver:
             raise Exception(f'version mismatch! __version__: {pace_ver}; {ver_name}: {ver}')
 
-    desc = re.search('# \[v[0-9\.]*\]\(http.*?\)\n(.*?)# \[v[0-9\.]*\]', changelog,
+    desc = re.search(r'# [v[0-9.]*[a-z0-9]{0,3}]\(http.*?\)\n(.*?)# [v[0-9.]*[a-z0-9]{0,3}]', changelog,
                      re.DOTALL | re.MULTILINE).group(1).strip()
     payload = {
         "tag_name": pace_ver,
